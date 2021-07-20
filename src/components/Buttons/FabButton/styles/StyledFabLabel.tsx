1764e87cd0e9ece53a3c59b2645e0f871b6c818b
KSQ <522981616@qq.com>
2021-07-20 10:03:15 +0800

    feat(Theme): fix token use

---
+++ src/components/Buttons/FabButton/styles/StyledFabLabel.tsx
@@ -0,0 +1,12 @@
+import { styled, grey } from '../../../../foundation';
+
+const StyledFabLabel = styled.span`
+  position: absolute;
+  display: flex;
+  align-items: flex-end;
+  top: 100%;
+  white-space: nowrap;
+  color: ${grey(800)};
+`;
+
+export { StyledFabLabel };
