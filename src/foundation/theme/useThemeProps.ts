5dccb36ef3def11440265de5ae441ca7136589f4
Alan Zou <itisalanlife@gmail.com>
2021-08-04 18:08:11 +0800

    doc:: add type for getScrollbarSize

--- src/foundation/theme/useThemeProps.ts
+++ src/foundation/theme/useThemeProps.ts
@@ -5,7 +5,7 @@ import { RcComponentsProps } from './theme.type';
 /**
  * use combine props from default theme props
  *
- * @see {@link https://ringcentral.github.io/juno/?path=/story/api-reference-hooks-usethemeprops--page 🔧-foundation-hooks-usethemeprops--use-theme-props-example}
+ * @see stories {@link https://ringcentral.github.io/juno/?path=/story/api-reference-hooks-usethemeprops--page 🔧-foundation-hooks-usethemeprops--use-theme-props-example}
  */
 export function useThemeProps<T, K extends keyof RcComponentsProps>({
   props,
