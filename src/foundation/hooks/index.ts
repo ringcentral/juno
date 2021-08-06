f1201bb8a5020d3e5c94ded3a16c8788efe8c887
Alan Zou <itisalanlife@gmail.com>
2021-08-06 14:20:24 +0800

    feat:: implement `useAudio` get Browser `Audio` instance, and auto destroy when component be destroyed.

--- src/foundation/hooks/index.ts
+++ src/foundation/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from './useA11yKeyEvent';
 export * from './useAnnouncer';
+export * from './useAudio';
 export * from './useChange';
 export * from './useDebounce';
 export * from './useEventCallback';
