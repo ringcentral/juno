95bcd990cac75fafe065e33f456c6299930b0cb1
Alan Zou <itisalanlife@gmail.com>
2021-08-17 22:32:31 +0800

    chore(storybook): fix storybook img path and remove version

--- .storybook/utils/customThemes.js
+++ .storybook/utils/customThemes.js
@@ -1,13 +1,11 @@
 import { create } from '@storybook/theming';
-import * as packageJSON from '../../package.json';
 
 const brandTitle = `
 <div style="display: flex; align-items: center;">
-  <img src="/assets/logo.png" style="width: 50px; margin-right: 1em;">
+  <img src="./assets/logo.png" style="width: 50px; margin-right: 1em;">
   <br />
   <div>
     <h1>Juno</h1>
-    <h3>v${packageJSON.version}</h3>
   </div>
 </div>
 `;
