7ba5fe5bc30fbe61781b66d9a7e1debacb26c404
Alan Zou <itisalanlife@gmail.com>
2021-08-17 21:56:02 +0800

    chore(package): add homepage url

--- scripts/build/writePackageJson.plugin.js
+++ scripts/build/writePackageJson.plugin.js
@@ -13,6 +13,9 @@ const writePackageJson = (pkg, targetPath) => {
         author: pkg.author,
         license: pkg.license,
         typings: pkg.typings,
+        bugs: pkg.bugs,
+        homepage: pkg.homepage,
+        repository: pkg.repository,
         dependencies: {
           ...pkg.dependencies,
           // that for type issue with @emotion type in styled-component type issue,
