9f835ddcddac99c82732dd5dd4aa159c1c8126bf
Alan Zou <itisalanlife@gmail.com>
2021-10-21 14:13:54 +0800

    feat(AppBar): cleanup AppBar

---
+++ src/components/AppBar/__stories__/AppBar.story.tsx
@@ -0,0 +1,52 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React, { ComponentProps } from 'react';
+
+import {
+  notControlInDocTable,
+  notShowInDocTable,
+  sortInDocTable,
+  switchToControlKnobs,
+} from '../../../storybook';
+import { RcAppBar } from '../AppBar';
+
+export default {
+  title: 'AppBar',
+  component: RcAppBar,
+  argTypes: {
+    ...sortInDocTable<keyof AppBarProps>([]),
+    ...notControlInDocTable<keyof AppBarProps>([]),
+    ...notShowInDocTable<keyof AppBarProps>([]),
+  },
+} as Meta;
+
+type AppBarProps = ComponentProps<typeof RcAppBar>;
+
+export const AppBar: Story<AppBarProps> = ({ children, ...args }) => {
+  switchToControlKnobs();
+  return <RcAppBar {...args}>{children}</RcAppBar>;
+};
+
+AppBar.storyName = 'AppBar';
+
+AppBar.args = {};
+
+AppBar.argTypes = {
+  ...notControlInDocTable<keyof AppBarProps>([]),
+};
+
+AppBar.parameters = {
+  tags: [
+    // {
+    //   name: 'Spec',
+    //   href: '',
+    // },
+    {
+      name: 'Mui',
+      href: 'https://material-ui.com/components/app-bar',
+    },
+    {
+      name: 'Accessibility',
+      value: '100%',
+    },
+  ],
+};
