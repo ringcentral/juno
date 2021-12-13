f8b4f5cab5854f23035997f0ee8a122044146301
233mawile <903945079@qq.com>
2021-12-13 10:57:12 +0800

    feat(withTooltip): fix withTooltip type error

--- src/components/Tooltip/withTooltip/withTooltip.tsx
+++ src/components/Tooltip/withTooltip/withTooltip.tsx
@@ -2,25 +2,25 @@ import React, { ComponentType, forwardRef } from 'react';
 
 import { RcTooltip, RcTooltipProps } from '../Tooltip';
 
-type WithTooltipProps = {
+type WithTooltipProps<P extends {} = {}> = {
   /** title on list root */
-  title?: RcTooltipProps['title'];
+  title?: RcTooltipProps['title'] | (P extends { title: infer T } ? T : never);
   /** html native title or not, default `false` */
   useRcTooltip?: boolean;
   /** props for pass into `RcTooltip` when useRcTooltip is `true` */
   TooltipProps?: Partial<RcTooltipProps>;
-};
+} & Omit<P, 'title'>;
 
 /**
  * Make a component can be use with `RcTooltip` and Tooltip Props
  */
 function withTooltip<T extends object>(Component: ComponentType<T>) {
-  return forwardRef<any, WithTooltipProps & T>((props, ref) => {
+  return forwardRef<any, WithTooltipProps<T>>((props, ref) => {
     const { title, useRcTooltip, TooltipProps, ...rest } = props;
 
     if (title && useRcTooltip) {
       return (
-        <RcTooltip title={title} {...TooltipProps}>
+        <RcTooltip title={title as RcTooltipProps['title']} {...TooltipProps}>
           <Component {...(rest as any)} ref={ref} />
         </RcTooltip>
       );
