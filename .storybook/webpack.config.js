b355124c289f47785f6b90cf66481bbcd26fe032
Alan Zou <itisalanlife@gmail.com>
2021-12-15 18:16:16 +0800

    chore(Package): add ts-loader back and upgrade storybook to v6.4.9

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,61 @@
 // @ts-nocheck
 const path = require('path');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
+
+const smp = new SpeedMeasurePlugin({ disable: !process.env.MEASURE });
 
 module.exports = async ({ config }) => {
   config = {
     ...config,
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.(ts|tsx)$/,
+          include: path.resolve(__dirname, '../src'),
+          use: [
+            {
+              loader: 'thread-loader',
+              options: {
+                // the number of spawned workers, defaults to (number of cpus - 1) or
+                // fallback to 1 when require('os').cpus() is undefined
+                workers: Number(process.env.TS_WORKER_NUMBER) || 8,
+
+                // number of jobs a worker processes in parallel
+                // defaults to 20
+                workerParallelJobs: 20,
+
+                // Allow to respawn a dead worker pool
+                // respawning slows down the entire compilation
+                // and should be set to false for development
+                poolRespawn: false,
+
+                // timeout for killing the worker processes when idle
+                // defaults to 500 (ms)
+                // can be set to Infinity for watching builds to keep workers alive
+                poolTimeout: 4000,
+
+                // number of jobs the poll distributes to the workers
+                // defaults to 200
+                // decrease of less efficient but more fair distribution
+                poolParallelJobs: 200,
+
+                // name of the pool
+                // can be used to create different pools with elsewise identical options
+                name: 'ts-worker-pool',
+              },
+            },
+            {
+              loader: 'ts-loader',
+              options: { happyPackMode: true, transpileOnly: true },
+            },
+          ],
+        },
+      ],
+    },
     plugins: [
       ...config.plugins,
       new CopyPlugin({
@@ -20,5 +70,5 @@ module.exports = async ({ config }) => {
     ],
   };
 
-  return config;
+  return smp.wrap(config);
 };
