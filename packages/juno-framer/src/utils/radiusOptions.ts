58e40a7fb892cf26ac6369fce6eab7413e3536f6
Alan Zou <itisalanlife@gmail.com>
2022-04-07 16:44:31 +0800

    fix(Framer): [Framer] update framer color options and iconOptions

--- packages/juno-framer/src/utils/radiusOptions.ts
+++ packages/juno-framer/src/utils/radiusOptions.ts
@@ -1,10 +1,3 @@
-export const radiusOptions = [
-  'circle',
-  'lg',
-  'md',
-  'round',
-  'sm',
-  'xl',
-  'xxl',
-  'zero',
-];
+import radius from '@ringcentral/juno/src/foundation/theme/assets/radius.json';
+
+export const radiusOptions = Object.keys(radius);
