65986d6a9224d147b5529098e1115a5b73b78332
Alan Zou <itisalanlife@gmail.com>
2022-04-18 16:49:13 +0800

    fix(foundation): [useTouchMouseEvent] remove addition check because in production should never switch between touch and mouse
    BREAKING CHNAGE: [useTouchMouseEvent] remove `isPreventDefault` because user can control event outside through function combine
    
    f

--- packages/juno-core/src/foundation/hooks/useTouchMouseEvent/useTouchMouseEvent.ts
+++ packages/juno-core/src/foundation/hooks/useTouchMouseEvent/useTouchMouseEvent.ts
@@ -1,16 +1,42 @@
 import { MouseEventHandler, TouchEventHandler, useRef } from 'react';
 
+import { useRcPortalWindowContext } from '../../contexts';
 import { getRefElement, RefOrElementOrCallback } from '../../utils';
 import { useEventCallback } from '../useEventCallback';
 
-export interface UseTouchMouseEvent<T> {
-  onMouseUp?: MouseEventHandler<T>;
-  onMouseDown?: MouseEventHandler<T>;
-  onTouchEnd?: TouchEventHandler<T>;
-  onTouchStart?: TouchEventHandler<T>;
+export interface UseTouchMouseEvent<T>
+  extends Partial<UseTouchMouseOutputEvent<T>> {
+  onTouchEnd?: (
+    event: React.TouchEvent<T>,
+    /**
+     * when set insideRef, that will also emit the state of host reference contain pointer element when action stop
+     */
+    isInside?: boolean,
+  ) => void;
+  onMouseUp?: (
+    event: React.MouseEvent<T, globalThis.MouseEvent>,
+    /**
+     * when set insideRef, that will also emit the state of host reference contain pointer element when action stop
+     */
+    isInside?: boolean,
+  ) => void;
 }
-export interface UseTouchMouseEventOptions {
-  isPreventDefault?: boolean;
+
+export interface UseTouchMouseOutputEvent<T> {
+  onMouseUp: MouseEventHandler<T>;
+  onMouseDown: MouseEventHandler<T>;
+  onTouchEnd: TouchEventHandler<T>;
+  onTouchStart: TouchEventHandler<T>;
+}
+
+export interface UseTouchMouseEventOptions<T extends HTMLElement> {
+  /**
+   * when set that elementRef, event will bring with leave state when `touchend` and `mouseup`
+   *
+   * stop action event in browser default can be trigger when `touchend`, `mouseup` outside target element,
+   * set that host ref to make sure `touchend`, `mouseup` event only trigger when release inside target element
+   */
+  actionRef?: RefOrElementOrCallback<T>;
 }
 /**
  * Provide a utils for switch trigger touch event or mouse event between different device
@@ -22,6 +48,7 @@ export interface UseTouchMouseEventOptions {
  *   - onMouseDown
  *   - onMouseUp
  *
+ * ### Once that is touch support device, that mouse event will never can be trigger
  * https://developer.mozilla.org/en-US/docs/Web/API/Touch_events/Supporting_both_TouchEvent_and_MouseEvent
  *
  * Event order:
@@ -33,67 +60,70 @@ export interface UseTouchMouseEventOptions {
  * - mouseup
  * - click
  */
-export const useTouchMouseEvent = <T = unknown>(
+export const useTouchMouseEvent = <T extends HTMLElement = HTMLElement>(
   {
     onMouseDown: onMouseDownArg,
     onMouseUp: onMouseUpArg,
     onTouchStart: onTouchStartArg,
     onTouchEnd: onTouchEndArg,
   }: UseTouchMouseEvent<T>,
-  { isPreventDefault = true }: UseTouchMouseEventOptions = {},
-) => {
-  const isLiveEndRef = useRef(false);
-  const isStartRef = useRef(false);
+  { actionRef }: UseTouchMouseEventOptions<T> = {},
+): UseTouchMouseOutputEvent<T> => {
+  const stopMouseEventRef = useRef(false);
 
-  const startAction = () => (isStartRef.current = true);
-  const endAction = () => (isStartRef.current = false);
+  const { document } = useRcPortalWindowContext();
 
   const onTouchStart = useEventCallback((e) => {
-    startAction();
-
+    stopMouseEventRef.current = true;
     onTouchStartArg?.(e);
   });
 
-  const onTouchEnd = useEventCallback((e) => {
-    endAction();
+  function checkInside(elm: HTMLElement) {
+    const hostElement = getRefElement(actionRef!);
 
-    isLiveEndRef.current = true;
-    onTouchEndArg?.(e);
+    if (hostElement) {
+      return !!(elm && isElmEqualOrContain(elm, hostElement));
+    }
+
+    return false;
+  }
+
+  const onTouchEnd = useEventCallback((e: React.TouchEvent<T>) => {
+    let isInside: boolean | undefined;
+
+    if (actionRef && e.touches.length < 2 && e.changedTouches.length < 2) {
+      const touch = e.touches[0] || e.changedTouches[0];
+
+      const elm = document.elementFromPoint(
+        touch.pageX,
+        touch.pageY,
+      ) as HTMLElement | null;
+
+      isInside = !!elm && checkInside(elm);
+    }
+
+    onTouchEndArg?.(e, isInside);
   });
 
-  const preventDefaultEvent = (e: Event) => {
-    if (isPreventDefault) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
-  };
-
-  const onMouseDown: MouseEventHandler<T> = useEventCallback((e) => {
-    startAction();
-
-    if (isLiveEndRef.current) {
-      e.preventDefault();
-      e.stopPropagation();
+  const onMouseDown = useEventCallback((e) => {
+    if (stopMouseEventRef.current) {
       return;
     }
     onMouseDownArg?.(e);
   });
 
   const onMouseUp = useEventCallback((e) => {
-    if (!isStartRef.current) {
-      preventDefaultEvent(e);
-      return;
+    let isInside: boolean | undefined;
+
+    if (stopMouseEventRef.current) return;
+
+    if (actionRef) {
+      const elm = e.target as HTMLElement | null;
+
+      isInside = !!elm && checkInside(elm);
     }
 
-    endAction();
-
-    if (isLiveEndRef.current) {
-      isLiveEndRef.current = false;
-      preventDefaultEvent(e);
-
-      return;
-    }
-    onMouseUpArg?.(e);
+    onMouseUpArg?.(e, isInside);
   });
 
   return {
