62f1b230ef793ab38857d3673f9eadde34e8c02b
233mawile <903945079@qq.com>
2022-04-26 09:57:54 +0800

    feat(FIJI-49778): [deepPartial] add dependency `type-fest` and use type-fest `PartialDeep` to replace current implementation

--- packages/juno-core/src/foundation/typings/deepPartial.ts
+++ packages/juno-core/src/foundation/typings/deepPartial.ts
@@ -1,10 +1,2 @@
-// https://gist.github.com/navix/6c25c15e0a2d3cd0e5bce999e0086fc9
-export type DeepPartial<T> = {
-  [P in keyof T]?: T[P] extends Array<infer U>
-    ? Array<DeepPartial<U>>
-    : T[P] extends ReadonlyArray<infer U>
-    ? ReadonlyArray<DeepPartial<U>>
-    : T[P] extends Record<any, any>
-    ? DeepPartial<T[P]>
-    : T[P];
-};
+// TODO: remove sub path after upgrade ts
+export type { PartialDeep as DeepPartial } from 'type-fest/source/partial-deep';
