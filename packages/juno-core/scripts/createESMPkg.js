e9f47b40a16afe65a055b6d54972f8c2a0fb5ae5
Neil.Gu <neil.gu@ringcentral.com>
2022-08-25 13:53:54 +0800

    chore(RCW-15888): [Make juno and jupiter-ui to support to be pre-bundled by Vite] add createESMPkg script to create package.json for all subpath

--- packages/juno-core/scripts/createESMPkg.js
+++ packages/juno-core/scripts/createESMPkg.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 /**
  * Puts a package.json into every immediate child directory of rootDir.
  * That package.json contains information about esm for bundlers so that imports
- * like import Typography from '@mui/material/Typography' are tree-shakeable.
  *
  * It also tests that an this import can be used in TypeScript by checking
  * if an index.d.ts is present at that path.
@@ -12,35 +11,31 @@ const fs = require('fs');
  * @param {string} param0.from
  * @param {string} param0.to
  */
-async function createESMPkg({ from, to }) {
+function createESMPkg({ from, to }) {
   from = path.resolve(__dirname, from)
   to = path.resolve(__dirname, to)
 
   const directoryPackages = glob
-    .sync('*/index.{js,ts,tsx}', { cwd: from })
+    .sync('*/**/index.{js,ts,tsx}', { cwd: from })
     .map(path.dirname);
 
   directoryPackages.push('');
 
-  await Promise.all(
-    directoryPackages.map(async (directoryPackage) => {
-      const directoryPackagePath = path.join(from, directoryPackage);
-      const packageJsonPath = path.join(directoryPackagePath, 'package.json');
+  directoryPackages.forEach(async (directoryPackage) => {
+    const directoryPackagePath = path.join(from, directoryPackage);
+    const packageJsonPath = path.join(directoryPackagePath, 'package.json');
 
-      const packageJson = {
-        sideEffects: false,
-        module: path.relative(
-          directoryPackagePath,
-          path.join(to, directoryPackage, 'index.js'),
-        ),
-        typings: './index.d.ts',
-      };
+    const packageJson = {
+      sideEffects: false,
+      module: path.relative(
+        directoryPackagePath,
+        path.join(to, directoryPackage, 'index.js'),
+      ),
+      typings: './index.d.ts',
+    };
 
-      fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-
-      return packageJsonPath;
-    }),
-  );
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+  })
 }
 
 createESMPkg({
