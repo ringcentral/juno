c45be85a1b1633ec0049aab0942bba2e614c4e90
233mawile <903945079@qq.com>
2022-10-18 17:21:47 +0800

    test(misc): [jest-base] fix ut crash when dist folder exist

--- jest-base.config.js
+++ jest-base.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   testURL: 'http://localhost',
   testPathIgnorePatterns: ['/node_modules/', '/build/', '/dist/'],
+  modulePathIgnorePatterns: ['/dist/'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       path.join(__dirname, './tests/__mocks__/fileMock.js'),
