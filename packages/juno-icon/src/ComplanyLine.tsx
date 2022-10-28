e4c56193aa23752b257ab04f495473e2efe4434a
233mawile <903945079@qq.com>
2022-10-28 10:21:35 +0800

    feat(FIJI-57851): [Icons] add icons ComplanyLine, DirectLine, ExtentionLine, Welcome

---
+++ packages/juno-icon/src/ComplanyLine.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { SVGProps, Ref, forwardRef, memo } from 'react';
+
+const ComplanyLine = memo(
+  forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+    <svg
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg"
+      ref={ref}
+      {...props}
+    >
+      <path d="M2 5v19.833h2.333V26c0 2.579 2.087 4.667 4.667 4.667S13.667 28.58 13.667 26v-1.167H30V5H11.333v21c0 1.39-.943 2.333-2.333 2.333S6.667 27.39 6.667 26v-1.167H9V5H2zm2.333 2.333h2.333V22.5H4.333V7.333zm9.334 0h14V22.5h-14V7.333zM16 9.667V12h9.333V9.667H16zm0 4.666v2.333h2.333v-2.333H16zm3.5 0v2.333h2.333v-2.333H19.5zm3.5 0v2.333h2.333v-2.333H23zm-7 3.5v2.333h2.333v-2.333H16zm3.5 0v2.333h2.333v-2.333H19.5zm3.5 0v2.333h2.333v-2.333H23z" />
+    </svg>
+  )),
+);
+ComplanyLine.displayName = 'ComplanyLine';
+ComplanyLine['iconName'] = 'complany-line';
+export default ComplanyLine;
---
+++ packages/juno-icon/src/DirectLine.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { SVGProps, Ref, forwardRef, memo } from 'react';
+
+const DirectLine = memo(
+  forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
+    <svg
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg"
+      ref={ref}
+      {...props}
+    >
+      <path d="M8 2v25.76c0 .586.206 1.186.63 1.61s1.024.63 1.61.63h11.2c.586 0 1.186-.206 1.61-.63s.63-1.024.63-1.61V7.6c0-.586-.206-1.186-.63-1.61s-1.024-.63-1.61-.63h-11.2V2H8zm2.24 5.6h11.2v4.48h-11.2V7.6zm0 6.72h11.2v13.44h-11.2V14.32zm1.12 2.24v2.24h2.24v-2.24h-2.24zm3.36 0v2.24h2.24v-2.24h-2.24zm3.36 0v2.24h2.24v-2.24h-2.24zm-6.72 3.36v2.24h2.24v-2.24h-2.24zm3.36 0v2.24h2.24v-2.24h-2.24zm3.36 0v2.24h2.24v-2.24h-2.24zm-6.72 3.36v2.24h2.24v-2.24h-2.24zm3.36 0v2.24h2.24v-2.24h-2.24zm3.36 0v2.24h2.24v-2.24h-2.24z" />
+    </svg>
+  )),
+);
+DirectLine.displayName = 'DirectLine';
+DirectLine['iconName'] = 'direct-line';
+export default DirectLine;
