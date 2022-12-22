a2d4490b0bea4e10c8b5f2c60ca9a48c7ace9f3b
233mawile <903945079@qq.com>
2022-12-22 16:49:33 +0800

    feat(FIJI-60106): [Icons] add icons Yelp, GoogleBusiness

--- packages/juno-icon/devUtils/svgToComponentMapping.ts
+++ packages/juno-icon/devUtils/svgToComponentMapping.ts
@@ -9,6 +9,8 @@ module.exports = {
   '7': '7',
   '8': '8',
   '9': '9',
+  GoogleBusiness: 'google-business',
+  Yelp: 'yelp',
   SwitchboardBorder: 'switchboard_border',
   Switchboard: 'switchboard',
   Scan: 'scan',
