d24b3acd3830592642cb44e9cb53669343c0f08a
233mawile <903945079@qq.com>
2023-01-16 13:17:40 +0800

    feat(JMX-1543): [useOnlyOneFocusable] remove retry logic of `focusIndex` BREAKING CHANGE: Remove `retryOptions` of useOnlyOneFocusable. If you need this, please use `useRetry` directly

--- packages/juno-core/src/foundation/hooks/useKeyboardMoveFocus/useOnlyOneFocusable.ts
+++ packages/juno-core/src/foundation/hooks/useKeyboardMoveFocus/useOnlyOneFocusable.ts
@@ -1,6 +1,5 @@
 import { combineProps } from '../../utils';
 import { useId } from '../useId';
-import { useRetry, UseRetryOptions } from '../useRetry';
 
 const MOVE_ATTR_KEY = 'data-move-focused';
 
@@ -9,8 +8,6 @@ export type UseOnlyOneFocusableParams = {
   focusedIndexRef: React.MutableRefObject<number>;
   /** wrap container for below all only have one focusable */
   containerRef: React.RefObject<HTMLElement>;
-  /** when focus fail that retry times option */
-  retryOptions?: UseRetryOptions;
 };
 
 /**
@@ -20,7 +17,6 @@ export type UseOnlyOneFocusableParams = {
 export const useOnlyOneFocusable = ({
   focusedIndexRef,
   containerRef,
-  retryOptions,
 }: UseOnlyOneFocusableParams) => {
   const moveFocusedId = useId(MOVE_ATTR_KEY, true);
 
@@ -42,19 +38,14 @@ export const useOnlyOneFocusable = ({
   };
 
   // * retry when not get target element
-  const { retry: focusIndexWithRetry } = useRetry((toIndex: number) => {
+  const focusIndex = (toIndex: number) => {
     const toElm = getIndexElm(toIndex);
-    if (toElm) {
-      toElm.focus();
-      return true;
-    }
-
-    return false;
-  }, retryOptions);
+    toElm?.focus();
+  };
 
   return {
     moveFocusedId,
-    focusIndex: focusIndexWithRetry,
+    focusIndex,
     setIndexTabIndex,
     getItemProps: (
       index: number,
