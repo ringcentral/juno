41449b874f21ac4bcb669050f5e9fe3c2aec77f8
233mawile <903945079@qq.com>
2023-03-20 12:31:49 +0800

    feat(Icon): add icon ExternalLinkGlobal, GoogleChromeLogo; remove icon PresenceAvailable, PresenceAvailable, PresenceDnd, PresenceOffline BREAKING CHANGE:remove icon PresenceAvailable, PresenceAvailable, PresenceDnd, PresenceOffline

--- packages/juno-icon/src/ExternalLinkGlobal.tsx
+++ packages/juno-icon/src/ExternalLinkGlobal.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { SVGProps, Ref, forwardRef, memo } from 'react';
 
-const PresenceAway = memo(
+const ExternalLinkGlobal = memo(
   forwardRef((props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
     <svg
       viewBox="0 0 32 32"
@@ -9,10 +9,10 @@ const PresenceAway = memo(
       ref={ref}
       {...props}
     >
-      <path d="M26.667 22H13.334a3.333 3.333 0 0 1-3.333-3.333V5.334a3.333 3.333 0 1 1 6.666 0v10h10a3.333 3.333 0 1 1 0 6.666z" />
+      <path d="M24 6.666v16h-2.666V11.23L7.886 24.68 6 22.794l13.46-13.46H8V6.666z" />
     </svg>
   )),
 );
-PresenceAway.displayName = 'PresenceAway';
-PresenceAway['iconName'] = 'presence-away';
-export default PresenceAway;
+ExternalLinkGlobal.displayName = 'ExternalLinkGlobal';
+ExternalLinkGlobal['iconName'] = 'external-link-global';
+export default ExternalLinkGlobal;
