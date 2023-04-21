2c86c88bb60e784f3e8d83d08a6679e12040bb96
233mawile <903945079@qq.com>
2023-04-22 00:05:05 +0800

    feat(JMX-2860): [useDebounce/useThrottle] support `leading` and `trailing` setting

--- packages/juno-core/src/foundation/hooks/useDebounce/useDebounce.ts
+++ packages/juno-core/src/foundation/hooks/useDebounce/useDebounce.ts
@@ -1,6 +1,7 @@
 import { useCallback, useEffect } from 'react';
 
 import debounce from 'lodash/debounce';
+import { DebounceSettings } from 'lodash';
 
 import { useEventCallback } from '../useEventCallback';
 
@@ -8,15 +9,20 @@ import { useEventCallback } from '../useEventCallback';
  * provide a debounce method for debounce method
  * @param fn method for debounce
  * @param debounceTime debounce timeout value, default is  `200`ms
+ * @param debounceSettings debounce setting object, more detail see `lodash`
  */
 export const useDebounce = <F extends (...args: any[]) => any>(
   fn: F,
   debounceTime: number = 200,
+  debounceSettings?: DebounceSettings,
 ) => {
   const memoFn = useEventCallback(fn);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const debounceFn = useCallback(debounce(memoFn, debounceTime), []);
+  const debounceFn = useCallback(
+    debounce(memoFn, debounceTime, debounceSettings),
+    [],
+  );
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => () => debounceFn.cancel(), []);
--- packages/juno-core/src/foundation/hooks/useThrottle/useThrottle.ts
+++ packages/juno-core/src/foundation/hooks/useThrottle/useThrottle.ts
@@ -1,6 +1,7 @@
 import { useCallback, useEffect } from 'react';
 
 import throttle from 'lodash/throttle';
+import { ThrottleSettings } from 'lodash';
 
 import { useEventCallback } from '../useEventCallback';
 
@@ -8,15 +9,20 @@ import { useEventCallback } from '../useEventCallback';
  * provide a throttle method for throttle method
  * @param fn method for throttle
  * @param throttleTime throttle timeout value, default is  `200`ms
+ * @param throttleSettings throttle setting object, more detail see `lodash`
  */
 export const useThrottle = <F extends (...args: any[]) => any>(
   fn: F,
-  debounceTime: number = 200,
+  throttleTime: number = 200,
+  throttleSettings?: ThrottleSettings,
 ) => {
   const memoFn = useEventCallback(fn);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const throttleFn = useCallback(throttle(memoFn, debounceTime), []);
+  const throttleFn = useCallback(
+    throttle(memoFn, throttleTime, throttleSettings),
+    [],
+  );
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => () => throttleFn.cancel(), []);
