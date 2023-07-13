35291e5e7b52dc2cfb59071a7d39750edaee8527
233mawile <903945079@qq.com>
2023-07-13 14:11:29 +0800

    fix(typography-warning): [typography] warning when typography is not existed

--- packages/juno-core/src/foundation/styles/typography.ts
+++ packages/juno-core/src/foundation/styles/typography.ts
@@ -1,6 +1,7 @@
 import { css } from '../styled-components';
 import { RcThemeProps } from '../theme/theme.type';
 import { RcTypographyKeys } from '../theme/typography.type';
+import { logInDev } from '../utils';
 
 /**
  * get typography key value with token
@@ -17,7 +18,16 @@ function typographyProp(name: RcTypographyKeys, key: string) {
   return ({ theme }: RcThemeProps) => {
     const themeOfTypography = theme.typography[name];
     if (typeof themeOfTypography !== 'object') {
-      throw new Error(`Unexpected typography name: ${name}`);
+      if (process.env.NODE_ENV !== 'production') {
+        throw new Error(`Unexpected typography name: ${name}`);
+      } else {
+        logInDev({
+          message: `Unexpected typography name: ${name}`,
+          component: 'typographyProp',
+          level: 'warn',
+        });
+        return undefined;
+      }
     }
     return themeOfTypography[key];
   };
